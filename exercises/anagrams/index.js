639d6718f7be42b1a2eb4b6c02fc769b443e5eff
Vu Vu <vuvu2259@gmail.com>
2021-02-24 23:34:00 +0700

    .

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function convertStr(str) {
+    str = str.trim().toLowerCase().replace(/ /g , '');
+    let tempArr = [];
+    for (let char of str) {
+        if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
+            tempArr.push(char);
+        }
+    }
+    str = tempArr.join('');
+    return str;
+}
+
+function hashStrToObj(str) {
+    let obj = {};
+    for (let char of str) {
+        obj[char] = obj[char] ? ++obj[char] : 1;
+    }
+    return obj;
+}
+
+function anagrams(stringA, stringB) {
+    stringA = convertStr(stringA);
+    stringB = convertStr(stringB);
+    let objA = hashStrToObj(stringA);
+    let objB = hashStrToObj(stringB);
+
+    for (const key in objA) {
+        if (objA[key] !== objB[key]) return false;
+    }
+    for (const key in objB) {
+        if (objA[key] !== objB[key]) return false;
+    }
+    return true;
+}
 
 module.exports = anagrams;
