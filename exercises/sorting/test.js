24aa04117a3e2fa8df0586f230557e649b2bf78a
Vu Vu <vuvu2259@gmail.com>
2021-03-04 01:29:41 +0700

    .

--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -29,9 +29,9 @@ describe('Merge sort', () => {
     const left = [1, 10];
     const right = [2, 8, 12];
 
-    expect(merge(left, right)).toEqual([1,2,8,10,12]);
+    expect(merge(left, right)).toEqual([1, 2, 8, 10, 12]);
   });
-  
+
   test('sorts an array', () => {
     expect(mergeSort(getArray())).toEqual(getSortedArray());
   });
